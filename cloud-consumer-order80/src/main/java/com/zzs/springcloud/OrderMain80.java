a8e69aabe339c2a57054d0b98873c35e7aaec527
Mountain <15627991561@139.com>
2021-01-16 17:56:23 +0800

    重写Ribbon的IRule接口,自定义轮询策略

--- cloud-consumer-order80/src/main/java/com/zzs/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/zzs/springcloud/OrderMain80.java
@@ -1,14 +1,17 @@
 package com.zzs.springcloud;
 
+import com.zzs.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author mountain
  * @since 2020/12/13 22:24
  */
 
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 @EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80 {
--- cloud-consumer-order80/src/main/java/com/zzs/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zzs/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.zzs.springcloud.controller;
 import com.zzs.springcloud.common.CommonResult;
 import com.zzs.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,17 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            //如果状态码为200
+            log.info(String.valueOf(entity.getHeaders()));
+            return entity.getBody();
+        }
+        return CommonResult.error(400, "操作失败");
     }
 }
 
