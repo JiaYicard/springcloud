fbcbf58bfb38870ea56742f946e1b31879c27eba
Mountain <15627991561@139.com>
2021-01-16 17:52:46 +0800

    order整合OpenFeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/zzs/springcloud/controller/OrderFeigController.java
@@ -0,0 +1,21 @@
+package com.zzs.springcloud.controller;
+
+import com.zzs.springcloud.common.CommonResult;
+import com.zzs.springcloud.entities.Payment;
+import com.zzs.springcloud.service.PaymentFeignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderFeigController {
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
