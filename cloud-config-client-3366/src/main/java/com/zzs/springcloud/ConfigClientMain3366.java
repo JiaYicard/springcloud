802479e94dd3d9213be6ae74101b977833863f31
Mountain <15627991561@139.com>
2021-03-16 17:41:52 +0800

    Config配置中心&Bus总线

---
+++ cloud-config-client-3366/src/main/java/com/zzs/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.zzs.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author mm013
+ * @Date 2021/3/16 10:08
+ */
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
